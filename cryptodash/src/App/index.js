838c5418c51d9dc98e39f9845cb87e61fc5f707e
Christian Guizar <c-guizar@hotmail.com>
2022-04-23 20:57:52 -0700

    refactor(extraCode): cut out unnecesary code

--- cryptodash/src/App/index.js
+++ cryptodash/src/App/index.js
@@ -1,21 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-import WelcomeMessage from './WelcomeMessage';
+
 import styled from 'styled-components';
 import AppLayout from './AppLayout'
 import AppBar from './AppBar';
 import {AppProvider} from './AppProvider'
 
-const MyButton = styled.div`
-  color: green;
-  ${props => props.primary && css`
-    color: palevioletred;
-    `}
-`
-const TomatoButton = styled(MyButton)`
-  color: tomato;
-  border-color: tomato;
-`
 
 function App() {
   return (
@@ -23,9 +13,6 @@ function App() {
       <AppProvider>
         <AppBar />
         <div className="App">
-          <WelcomeMessage/>
-          <MyButton primary> Check </MyButton>
-          <TomatoButton>Check check</TomatoButton>
         </div>
       </AppProvider>
     </AppLayout>2
