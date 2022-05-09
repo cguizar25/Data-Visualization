c33ad7a1746d768fa1c428ca498cd8f462af3224
Christian Guizar <c-guizar@hotmail.com>
2022-05-09 10:29:55 -0700

    feat(coinGrid): added a grid of the available coins

--- cryptodash/src/Settings/index.js
+++ cryptodash/src/Settings/index.js
@@ -1,10 +1,12 @@
 import WelcomeMessage from './WelcomeMessage';
 import ConfineButton from './ConfineButton';
 import Page from '../Shared/Page';
+import CoinGrid from './CoinGrid';
 
 export default function(){
   return <Page name="settings">
     <WelcomeMessage />
     <ConfirmButton />
+    <CoinGrid />
   </Page>
 }
