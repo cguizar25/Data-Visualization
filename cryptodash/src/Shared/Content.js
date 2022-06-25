f7521ca5d4f604d39eac9b35710008d0d5130767
Christian Guizar <christianguizar@macbook-pro.lan>
2022-06-24 21:32:30 -0700

    feat(prices): added prices to coin

--- cryptodash/src/Shared/Content.js
+++ cryptodash/src/Shared/Content.js
@@ -2,9 +2,12 @@ import { AppContext } from '../App/AppProvider';
 
 export default function (props) {
   return <AppContext.Consumer>
-    {({coinList}) => {
+    {({coinList, prices, firstVisit}) => {
       if(!coinList){
         return <div> Loading Coins </div>
+      }
+      if(!firstVisit && !prices) {
+        return <div> Loading prices </div>
       }
       return <div> {props.children} </div>
     }}
