5f87af5600744bc4679141285896484a92b0eccb
Christian Guizar <christianguizar@macbook-pro.lan>
2022-06-26 21:06:01 -0700

    feat(dashboard): added basic dashboard

--- cryptodash/src/App/index.js
+++ cryptodash/src/App/index.js
@@ -5,6 +5,7 @@ import AppLayout from './AppLayout'
 import AppBar from './AppBar';
 import {AppProvider} from './AppProvider'
 import Settings from '../Settings';
+import Dashboard from '../Dashboard';
 import Content from '../Shared/Content';
 
 
@@ -15,6 +16,7 @@ function App() {
         <AppBar />
         <Content>
           <Settings />
+          <Dashboard />
         </Content>
       </AppProvider>
     </AppLayout>
