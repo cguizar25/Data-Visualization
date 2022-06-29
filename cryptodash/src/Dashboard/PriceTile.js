e70818ebcab7031983686891a28639d4e7d258fa
Christian Guizar <christianguizar@macbook-pro.lan>
2022-06-28 17:01:25 -0700

    style(dashboard)

--- cryptodash/src/Dashboard/PriceTile.js
+++ cryptodash/src/Dashboard/PriceTile.js
@@ -1,8 +1,27 @@
 import React from 'react';
 import styled, { css } from 'styled-components';
 import { SelectableTile } from '../Shared/Tile';
-import { fontSize3 } from '../Shared/Styles';
+import { fontSize3, fontSizeBig } from '../Shared/Styles';
 import { CoinHeaderGridStyled } from '../Settings/CoinHeaderGridStyled';
+
+const JustifyRight = styled.div`
+  justify-self: right;
+`
+
+const JustifyLeft = styled.div`
+  justify-self: left;
+`
+
+const TicketPrice = styled.div`
+  ${fontSizeBigs};
+`
+
+const ChangePct = styled.div`
+  color: green;
+  ${props => props.red && css`
+    color: red
+  `}
+`
 
 const numberFormat = number => {
   return +(number + '').slice(0, 7);
@@ -10,23 +29,53 @@ const numberFormat = number => {
 
 const PriceTileStyled = syled(SelectableTile)`
   ${props => props.compact && css`
+    display: grid;
     ${fontSize3}
+    grid-gap: 5px;
+    grid-template-columns: repeat(3, 1fr);
+    justify-items: right;
   `}
 `_
+
+function ChangePercent({data}){
+  return (
+    <JustifyRight>
+      <ChangePct red={data.CHANGEPCT24HOUR < 0}>
+        {numberFormat(data.CHANGEPCT24HOUR)}
+      </ChangePct>
+    </JustifyRight>
+  )
+}
 
 function PriceTile({sym, data}) {
   return (
     <PriceTileStyled>
       <CoinHeaderGridStyled>
         <div> {sym} </div>
-        <div> {numberFormat(data.CHANGEPCT24HOUR)} </div>
+        <ChangePercent data={data} />
       </CoinHeaderGridStyled>
+      <TicketPrice
+        ${numberFormat(data.Price)}
+      </TicketPrice>
     </PriceTileStyled>
-  )
+  );
+}
+
+function PriceTileCompact({sym, data}) {
+  <PriceTileStyled compact>
+    <JustifyLeft>
+      {sym}
+    </JustifyLeft>
+    <ChangePercent data={data} />
+    <div>
+      ${numberFormat(data.Price)}
+    </div>
+  </PriceTileStyled>
 }
 
 export default function({price, index}){
   let sym = Object.keys(price)[0];
   let data = price[sym]['USD'];
-  return <PriceTile sym={sym} data={data.PRICE}> {sym} {data.PRICE} </PriceTile>
+  let TileClass = index < 5 ? PriceTile: PriceTileCompact;
+  return <TileClass sym={sym} data={data.PRICE}> {sym} {data.PRICE} </TileClass>
 }
