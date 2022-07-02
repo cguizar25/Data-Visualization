428d69f69b46c80aaea1038e121a50afc65d4578
Christian Guizar <christianguizar@MacBook-Pro.attlocal.net>
2022-07-02 09:46:40 -0700

    feat(coinSpotlight): added coin spotlight feature

--- cryptodash/src/Shared/CoinImage.js
+++ cryptodash/src/Shared/CoinImage.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import styled, { css } from 'styled-components';
+
+const CoinImage = styled.img`
+  height: 50px;
+  ${props => props.spotlight && css`
+    margin: auto;
+    height: 200px;
+  `}
+`_
 
 export default function ({coin, style}) {
   return <img
+    spotlight={spotlight}
     alt={coin.CoinSymbol}
-    styled={style || {height: '50px'}}
     src={`http://cryptocompare.com/${
       coin.ImageUrl
     }`}
